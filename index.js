5c8723b2e640657220f024f6858c803d679b3ef4
kaleabd <kaleabdereje94@gmail.com>
2023-04-04 07:10:59 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+var user_name = "names";
+var user_age = 34;
+console.log(user_name, user_age);
---
+++ index.ts
@@ -0,0 +1,3 @@
+const user_name: string = "names"
+const user_age: number = 34
+console.log(user_name, user_age)
